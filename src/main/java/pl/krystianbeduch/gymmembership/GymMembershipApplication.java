f8a33f6f14a07d2803ccbd2067e8ee796401e035
Krystian Będuch <beduch_krystian@o2.pl>
2026-05-01 00:23:33 +0200

    init commit

---
+++ src/main/java/pl/krystianbeduch/gymmembership/GymMembershipApplication.java
@@ -0,0 +1,13 @@
+package pl.krystianbeduch.gym_membership_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymMembershipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymMembershipApplication.class, args);
+	}
+
+}
